cec49c1d45cbf0cd5030199d6ec8f1c414a104f4
Gerrudo <tomturner279@gmail.com>
2024-06-04 08:01:05 +0100

    Removed modifier calculations, simplified resource management for power and workers.

--- Assets/Scripts/SimulationManager.cs
+++ Assets/Scripts/SimulationManager.cs
@@ -9,12 +9,9 @@ public class SimulationManager : MonoBehaviour
     public int day;
     public int bricks;
     public int population;
-    public int workers;
-    public int maxWorkers;
-    public int energy;
-    public int maxEnergyProduction;
-    public int maxEnergyConsumption;
-    public decimal totalBrickModifier;
+    public int jobs;
+    public int energyProduction;
+    public int energyConsumption;
 
     public TextMeshProUGUI statsText;
 
@@ -48,74 +45,26 @@ public class SimulationManager : MonoBehaviour
         }
     }
 
-    void CalculateWorkers()
+    void CalculateJobs()
     {
-        workers = 0;
-        maxWorkers = 0;
+        //workers is not accounted for, as it is the total population.
+        jobs = 0;
 
         foreach (BuildingPreset building in buildings)
         {
-            maxWorkers += building.jobs;
-        }
-
-        if (population > maxWorkers)
-        {
-            workers = maxWorkers;
-        }
-        else if (population < maxWorkers)
-        {
-            workers = population;
+            jobs += building.jobs;
         }
     }
 
     void CalculateEnergy()
     {
-        energy = 0;
-        maxEnergyProduction = 0;
-        maxEnergyConsumption = 0;
+        energyProduction = 0;
+        energyConsumption = 0;
 
         foreach (BuildingPreset building in buildings)
         {
-            maxEnergyProduction += building.energyProduction;
-            maxEnergyConsumption += building.energyConsumption;
-        }
-
-        energy = (maxEnergyProduction - maxEnergyConsumption);
-    }
-
-    void CalculateModifiers()
-    {
-        decimal totalBrickModifier = 1;
-
-        List<decimal> modiifiers = new List<decimal>();
-
-        //To not divide by zero.
-        if (workers > 0)
-        {
-            decimal workerModifier = (decimal)workers / (decimal)maxWorkers;
-
-            modiifiers.Add(workerModifier);
-        }
-
-        //To not divide by zero.
-        if (maxEnergyProduction > 0)
-        {
-            decimal energyModifer = (decimal)maxEnergyProduction / (decimal)maxEnergyConsumption;
-
-            modiifiers.Add(energyModifer);
-        }
-
-        foreach (var modiifier in modiifiers)
-        {
-            //The modifier cannot be greater than 100% production rate.
-            if (modiifier < 1)
-            {
-                totalBrickModifier += modiifier / modiifiers.Count;
-            }
-            else
-            {
-                totalBrickModifier += 1 / modiifiers.Count;
-            }
+            energyProduction += building.energyProduction;
+            energyConsumption += building.energyConsumption;
         }
     }
 
@@ -123,24 +72,37 @@ public class SimulationManager : MonoBehaviour
     {
         int maxBrickProduction = 0;
 
+        if (energyProduction < energyConsumption)
+        {
+            Debug.Log("Not enough power to produce bricks.");
+
+            return;
+        }
+
+        if (population < jobs)
+        {
+            Debug.Log("Not enough workers to produce bricks.");
+
+            return;
+        }
+
         foreach (BuildingPreset building in buildings)
         {
             maxBrickProduction += building.brickProduction;
         }
 
-        bricks += maxBrickProduction * totalBrickModifier;
+        bricks += maxBrickProduction;
     }
 
     void UpdateStatistics()
     {
         day++;
         CalcuatePopulation();
-        CalculateWorkers();
+        CalculateJobs();
         CalculateEnergy();
-        CalculateModifiers();
         CalculateBricks();
 
         //I copied this from a tutorial and I hate it, surely there's a way of casting variables to UI text better than this
-        statsText.text = string.Format("Day: {0}   Bricks: {1}   Population: {2}   Energy: {3}   Workers: {4}    Production Rate: {5}", new object[6] { day, bricks, population, energy, workers, totalBrickModifier });
+        statsText.text = string.Format("Day: {0}   Bricks: {1}   Population: {2}   Energy: {3}/{4}   Workers: {5}/{6}", new object[7] { day, bricks, population, energyProduction, energyConsumption, population, jobs });
     }
 }
